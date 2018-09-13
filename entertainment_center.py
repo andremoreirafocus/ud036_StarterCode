5910b9962be769d9302bb39a24e8323257be7f18
andremoreirafocus <39835936+andremoreirafocus@users.noreply.github.com>
2018-09-13 16:22:55 -0300

    Create entertainment_center.py

---
+++ entertainment_center.py
@@ -0,0 +1,18 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "A movie that shows the life of a boy´s toys",
+                        "https://upload.wikimedia.org/wikipedia/pt/d/dc/Movie_poster_toy_story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
+
+#toy_story.show_trailer()
+
+movies = [toy_story]
+
+print(media.Movie.VALID_RATINGS)
+#fresh_tomatoes.open_movies_page(movies)
+
+
